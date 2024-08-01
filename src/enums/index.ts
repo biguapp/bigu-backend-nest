3277b830b18dc3b05aa9db709098d37e44c48f8d
Italo Ramalho <italo.ramalho@ccc.ufcg.edu.br>
2024-08-01 13:49:01 -0300

    criaçao de módulo para armazenar os enums

---
+++ src/enums/index.ts
@@ -0,0 +1,29 @@
+export enum HealthUnitType {
+  UPA = 'UPA',
+  UBS = 'UBS',
+  HOSPITAL = 'HOSPITAL',
+  // Adicione outros tipos conforme necessário
+}
+
+export enum ServiceType {
+  CLINICA_GERAL = 'Clínica Geral',
+  CARDIOLOGIA = 'Cardiologia',
+  DERMATOLOGIA = 'Dermatologia',
+  ENDOCRINOLOGIA = 'Endocrinologia',
+  GASTROENTEROLOGIA = 'Gastroenterologia',
+  GERIATRIA = 'Geriatria',
+  GINECOLOGIA = 'Ginecologia',
+  NEFROLOGIA = 'Nefrologia',
+  NEUROLOGIA = 'Neurologia',
+  OBSTETRICIA = 'Obstetrícia',
+  OFTALMOLOGIA = 'Oftalmologia',
+  ONCOLOGIA = 'Oncologia',
+  ORTOPEDIA = 'Ortopedia',
+  OTORRINOLARINGOLOGIA = 'Otorrinolaringologia',
+  PEDIATRIA = 'Pediatria',
+  PNEUMOLOGIA = 'Pneumologia',
+  PSIQUIATRIA = 'Psiquiatria',
+  RADIOLOGIA = 'Radiologia',
+  REUMATOLOGIA = 'Reumatologia',
+  UROLOGIA = 'Urologia',
+}
