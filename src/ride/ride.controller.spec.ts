f7f7d38828edd58019e6de2acf6e30244fd15af9
Ítalo Ramalho <italo.ramalho@ccc.ufcg.edu.br>
2024-09-23 21:32:03 -0300

    inicio da implementação de testes do rideController com erro

--- src/ride/ride.controller.spec.ts
+++ src/ride/ride.controller.spec.ts
@@ -4,24 +4,97 @@ import { RideService } from './ride.service';
 import { getModelToken } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Ride } from './schemas/ride.schema';
-import { UserService } from '../user/user.service'; 
+import { UserService } from '../user/user.service';
 import { Candidate } from './schemas/candidate.schema';
 import { Member } from './schemas/member.schema';
 import { MailjetService } from 'nest-mailjet';
 import { AuthService } from '../auth/auth.service';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { CreateRideDto } from './dto/create-ride.dto';
+import { RideResponseDto } from './dto/response-ride.dto';
 
 describe('RideController', () => {
   let controller: RideController;
   let service: RideService;
   let model: Model<Ride>;
 
+  const mockRideDto: CreateRideDto = {
+    driver: '1',
+    startAddress: 'Start Location',
+    destinationAddress: 'Destination Location',
+    numSeats: 4,
+    goingToCollege: true,
+    price: 20.0,
+    scheduledTime: '2024-09-10T15:30:00Z',
+    car: 'Car Model',
+    description: 'Ride to campus',
+    toWomen: false,
+  };
+
+  const rideResponse: RideResponseDto = {
+    rideId: '123',
+    driver: {
+      name: 'João Silva',
+      matricula: '2021001234',
+      sex: 'Masculino',
+      email: 'joao.silva@example.ufcg.edu.br',
+      phoneNumber: '(83) 99999-9999',
+      userId: '1',
+      avgScore: 5,
+      feedbacks: [],
+      isVerified: true,
+    },
+    startAddress: {
+      rua: 'Avenida Paulista',
+      nome: 'Casa',
+      numero: '1234',
+      complemento: 'Apartamento 56',
+      bairro: 'Bela Vista',
+      cidade: 'São Paulo',
+      estado: 'SP',
+      addressId: '1',
+    },
+    destinationAddress: {
+      rua: 'Avenida Paulista',
+      nome: 'Casa',
+      numero: '1234',
+      complemento: 'Apartamento 56',
+      bairro: 'Bela Vista',
+      cidade: 'São Paulo',
+      estado: 'SP',
+      addressId: '2',
+    },
+    numSeats: 3,
+    goingToCollege: true,
+    price: 15.0,
+    scheduledTime: '2024-09-10T15:30:00Z',
+    car: {
+      brand: 'Chevrolet',
+      carModel: 'Onix',
+      modelYear: '2019',
+      color: 'Preto',
+      plate: 'KGU7E07',
+      carId: '1',
+    },
+    description: 'Carona para o campus universitário',
+    toWomen: false,
+    members: [],
+    candidates: [],
+    isOver: false,
+  };
+
   // Mock do RideService
   const mockRideService = {
-    create: jest.fn().mockResolvedValue({ /* mock de uma corrida */ } as Ride),
-    findAll: jest.fn().mockResolvedValue([{ /* mock de corridas */ }] as Ride[]),
-    findOne: jest.fn().mockResolvedValue({ /* mock de uma corrida */ } as Ride),
-    update: jest.fn().mockResolvedValue({ /* mock de uma corrida atualizada */ } as Ride),
+    create: jest.fn().mockResolvedValue({
+      ...mockRideDto,
+      toDTO: jest.fn().mockReturnValue(mockRideDto),
+    }),
+    findAll: jest.fn().mockResolvedValue([mockRideDto, mockRideDto]),
+    findOne: jest.fn().mockResolvedValue({
+      ...rideResponse,
+      toDTO: jest.fn().mockReturnValue(rideResponse),
+    }),
+    update: jest.fn().mockReturnThis(),
     remove: jest.fn().mockResolvedValue(undefined),
     removeAll: jest.fn().mockResolvedValue(undefined),
   };
@@ -31,17 +104,11 @@ describe('RideController', () => {
     // Adicione métodos mockados do UserService, se necessário
   };
 
-  const mockMemberModel = {
+  const mockMemberModel = {};
 
-  };
+  const mockCandidateModel = {};
 
-  const mockCandidateModel = {
-
-  };
-
-  const mockMailjetService = {
-    
-  };
+  const mockMailjetService = {};
 
   const mockAuthService = {
     validateUser: jest.fn().mockResolvedValue(true),
@@ -51,14 +118,23 @@ describe('RideController', () => {
     canActivate: jest.fn(() => true), // Permite a ativação do guard nas rotas protegidas
   };
 
+  const response = {
+    status: jest.fn().mockReturnThis(),
+    json: jest.fn(),
+  } as unknown as Response;
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [RideController],
       providers: [
-        RideService,
+        {
+          provide: RideService,
+          useValue: mockRideService,
+        },
+        
         {
           provide: getModelToken(Ride.name),
-          useValue: model, // Use a instância de modelo mockada se necessário
+          useValue: Model, // Use a instância de modelo mockada se necessário
         },
         {
           provide: getModelToken(Member.name),
@@ -80,20 +156,65 @@ describe('RideController', () => {
           provide: AuthService,
           useValue: mockAuthService, // Mock AuthService
         },
-        
       ],
-    }).overrideGuard(JwtAuthGuard) // Sobrescreve o guard para evitar erros
-    .useValue(mockJwtAuthGuard) // Mock JwtAuthGuard
-    .compile();
+    })
+      .overrideGuard(JwtAuthGuard) // Sobrescreve o guard para evitar erros
+      .useValue(mockJwtAuthGuard) // Mock JwtAuthGuard
+      .compile();
 
     controller = module.get<RideController>(RideController);
     service = module.get<RideService>(RideService);
-    model = module.get<Model<Ride>>(getModelToken(Ride.name));
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
 
-  // Adicione mais testes aqui
+  describe('create', () => {
+    it('should create a ride and return it', async () => {
+      const createRideDto: CreateRideDto = { ...mockRideDto };
+
+      await controller.create(createRideDto, response);
+
+      expect(service.create).toHaveBeenCalledWith(createRideDto);
+      expect(response.json).toHaveBeenCalledWith({
+        message: 'A carona foi criada com sucesso.',
+        newRide: { ...createRideDto },
+      });
+    });
+  });
+
+  describe('findAll', () => {
+    it('should return all rides', async () => {
+      const rides = await service.findAll();
+      expect(rides).toEqual([mockRideDto, mockRideDto]);
+    });
+  });
+
+  describe('findOne', () => {
+    it('should return one ride by ID', async () => {
+      const rideId = '123';
+
+      await controller.findOne(rideId, response);
+
+      expect(service.findOne).toHaveBeenCalledWith(rideId);
+      expect(response.json).toHaveBeenCalledWith({
+        message: 'A carona foi retornada com sucesso.',
+        ride: rideResponse,
+      });
+    });
+
+    it('should return 404 if ride not found', async () => {
+      const rideId = 'non-existent-id';
+      mockRideService.findOne.mockResolvedValueOnce(null); // Simula o comportamento de "não encontrado"
+
+      await controller.findOne(rideId, response);
+
+      expect(service.findOne).toHaveBeenCalledWith(rideId);
+      expect(response.status).toHaveBeenCalledWith(404);
+      expect(response.json).toHaveBeenCalledWith({
+        message: 'Carona não encontrada.',
+      });
+    });
+  });
 });
