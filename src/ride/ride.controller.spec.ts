6bd6906ac2d0cad0ef499b6456e6a442b4288be0
luisaadanttas <maria.luisa.dantas@ccc.ufcg.edu.br>
2024-09-23 21:06:16 -0300

    teste

--- src/ride/ride.controller.spec.ts
+++ src/ride/ride.controller.spec.ts
@@ -4,50 +4,126 @@ import { RideService } from './ride.service';
 import { getModelToken } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Ride } from './schemas/ride.schema';
-import { UserService } from '../user/user.service'; // Ajuste o caminho se necessário
-import { AddressService } from '../address/address.service';
+import { CreateRideDto } from './dto/create-ride.dto';
+import { AuthService } from '../auth/auth.service';
+import { RideResponseDto } from './dto/response-ride.dto';
 
 describe('RideController', () => {
   let controller: RideController;
   let service: RideService;
   let model: Model<Ride>;
 
-  // Mock do RideService
+  const mockRideDto : CreateRideDto = {
+    driver: '1',
+    startAddress: 'Start Location',
+    destinationAddress: 'Destination Location',
+    numSeats: 4,
+    goingToCollege: true,
+    price: 20.0,
+    scheduledTime: '2024-09-10T15:30:00Z',
+    car: 'Car Model',
+    description: 'Ride to campus',
+    toWomen: false,
+  };
+
+  const rideResponse: RideResponseDto = {
+    rideId: '123',
+    driver: {
+      name: 'João Silva',
+      matricula: '2021001234',
+      sex: 'Masculino',
+      email: 'joao.silva@example.com',
+      phoneNumber: '(83) 99999-9999',
+      userId: '1',
+      avgScore: 4.8,
+      feedbacks: [
+        'Ótimo motorista, muito pontual.',
+        'A viagem foi tranquila e confortável.'
+      ],
+      isVerified: true,
+    },
+    startAddress: {
+        rua: 'Avenida Paulista',
+        nome: 'Casa',
+        numero: '1234',
+        complemento: 'Apartamento 56',
+        bairro: 'Bela Vista',
+        cidade: 'São Paulo',
+        estado: 'SP',
+        addressId: '1',
+    },
+    destinationAddress: {
+
+        rua: 'Avenida Paulista',
+        nome: 'Casa',
+        numero: '1234',
+        complemento: 'Apartamento 56',
+        bairro: 'Bela Vista',
+        cidade: 'São Paulo',
+        estado: 'SP',
+        addressId: '1',
+      
+    },
+    numSeats: 3,
+    goingToCollege: true,
+    price: 15.0,
+    scheduledTime: '2024-09-10T15:30:00Z',
+    car: {
+      brand: 'Chevrolet',
+      carModel: 'Onix',
+      modelYear: '2019',
+      color: 'Preto',
+      plate: 'KGU7E07',
+      carId: '1',
+    },
+    description: 'Carona para o campus universitário',
+    toWomen: false,
+    members: [
+    ],
+    candidates: [
+    ],
+    isOver: false,
+  };
+  
   const mockRideService = {
-    create: jest.fn().mockResolvedValue({ /* mock de uma corrida */ } as Ride),
-    findAll: jest.fn().mockResolvedValue([{ /* mock de corridas */ }] as Ride[]),
-    findOne: jest.fn().mockResolvedValue({ /* mock de uma corrida */ } as Ride),
-    update: jest.fn().mockResolvedValue({ /* mock de uma corrida atualizada */ } as Ride),
+    create: jest.fn().mockResolvedValue({
+      ...mockRideDto,
+      toDTO: jest.fn().mockReturnValue(mockRideDto),
+    }),
+    findAll: jest.fn().mockResolvedValue([mockRideDto, mockRideDto]),
+    findOne: jest.fn().mockResolvedValue({
+      ...rideResponse,
+      toDTO: jest.fn().mockReturnValue(rideResponse),
+    }),
+    update: jest.fn().mockReturnThis(),
     remove: jest.fn().mockResolvedValue(undefined),
     removeAll: jest.fn().mockResolvedValue(undefined),
   };
 
-  // Mock do UserService (se necessário)
-  const mockUserService = {
-    // Adicione métodos mockados do UserService, se necessário
-  };
-
-  const mockAddressService = {
-
-  }
+  const mockAuthService = {
+    validateUser: jest.fn().mockResolvedValue(true),
+  };  
+  const response = {
+    status: jest.fn().mockReturnThis(),
+    json: jest.fn(),
+  } as unknown as Response;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [RideController],
       providers: [
-        RideService,
+        {
+          provide: RideService,
+          useValue: mockRideService,
+        },
         {
           provide: getModelToken(Ride.name),
-          useValue: model, // Use a instância de modelo mockada se necessário
+          useValue: model,
         },
         {
-          provide: UserService,
-          useValue: mockUserService,
+          provide: AuthService,
+          useValue: mockAuthService,
         },
-        {
-          provide: AddressService,
-          useValue: mockAddressService,
-        }
       ],
     }).compile();
 
@@ -56,9 +132,44 @@ describe('RideController', () => {
     model = module.get<Model<Ride>>(getModelToken(Ride.name));
   });
 
+  describe('define', () => {
   it('should be defined', () => {
     expect(controller).toBeDefined();
+    });
   });
 
-  // Adicione mais testes aqui
+  describe('create', () => {
+  it('should create a ride and return it', async () => {
+    const createRideDto: CreateRideDto = { ...mockRideDto };
+
+    await controller.create(createRideDto, response);
+    
+    expect(service.create).toHaveBeenCalledWith(createRideDto);
+    expect(response.json).toHaveBeenCalledWith({
+      message: "A carona foi criada com sucesso.",
+      newRide: { ...createRideDto },
+    });
+  })});
+  
+  describe('findAll', () => {
+  it('should return all rides', async () => {
+    const rides = await service.findAll();
+    expect(rides).toEqual([mockRideDto,mockRideDto]);
+    });
+  });
+
+  describe('findOne', () => {
+  it('should return one ride', async () => {
+    const rideId = '23';
+  
+    await controller.findOne(rideId, response);
+
+    expect(service.findOne).toHaveBeenCalledWith(rideId);
+    expect(response.json).toHaveBeenCalledWith({
+      message: "A carona foi retornada com sucesso.",
+      ride: [],
+      });
+    });
+  });
+
 });
--- src/ride/ride.service.spec.ts
+++ src/ride/ride.service.spec.ts
@@ -3,8 +3,9 @@ import { RideService } from './ride.service';
 import { getModelToken } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Ride } from './schemas/ride.schema';
-import { UserService } from '../user/user.service'; // Ajuste o caminho se necessário
+import { UserService } from '../user/user.service';
 import { AddressService } from '../address/address.service';
+import { MailjetService } from 'nest-mailjet';
 
 describe('RideService', () => {
   let service: RideService;
@@ -14,17 +15,33 @@ describe('RideService', () => {
   const mockRideModel = {
     find: jest.fn().mockResolvedValue([]), // Exemplo de método mockado
     findById: jest.fn().mockResolvedValue(null),
+    findOne: jest.fn().mockResolvedValue({/* mock user data */}),
+
     create: jest.fn().mockResolvedValue({}),
     // Adicione outros métodos conforme necessário
   };
-
+  const mockMailjetService = {
+    sendEmail: jest.fn().mockResolvedValue(true), // Example of a mocked method
+  };
   // Mock do UserService
   const mockUserService = {
+    findOne: jest.fn().mockResolvedValue({/* mock user data */}),
     // Adicione métodos mockados do UserService, se necessário
   };
 
+  // Mock do AddressService
   const mockAddressService = {
+    // Adicione métodos mockados do AddressService, se necessário
+  };
 
+  // Mock do MemberModel
+  const mockMemberModel = {
+    // Adicione métodos mockados do MemberModel, se necessário
+  };
+
+  // Mock do CandidateModel
+  const mockCandidateModel = {
+    // Adicione métodos mockados do CandidateModel, se necessário
   };
 
   beforeEach(async () => {
@@ -36,13 +53,25 @@ describe('RideService', () => {
           useValue: mockRideModel,
         },
         {
+          provide: MailjetService,
+          useValue: mockMailjetService,
+        },
+        {
+          provide: getModelToken('Member'),
+          useValue: mockMemberModel,
+        },
+        {
+          provide: getModelToken('Candidate'),
+          useValue: mockCandidateModel,
+        },
+        {
           provide: UserService,
           useValue: mockUserService,
         },
         {
           provide: AddressService,
           useValue: mockAddressService,
-        }
+        },
       ],
     }).compile();
 
