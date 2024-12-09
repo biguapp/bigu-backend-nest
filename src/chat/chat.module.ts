59ba2878e5c2f7349131f35914753815ce5a3e18
MARIA DANTAS <maria@MARIAs-MacBook-Air.local>
2024-12-09 14:01:45 -0300

    fix chat module

--- src/chat/chat.module.ts
+++ src/chat/chat.module.ts
@@ -3,13 +3,15 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ChatController } from './chat.controller';
 import { ChatService } from './chat.service';
 import { ChatSchema } from './schemas/chat.schema';
+import { RideModule } from '@src/ride/ride.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: 'Chat', schema: ChatSchema }]),
+    RideModule, 
   ],
   controllers: [ChatController],
   providers: [ChatService],
-  exports: [ChatService], 
+  exports: [ChatService],
 })
 export class ChatModule {}
